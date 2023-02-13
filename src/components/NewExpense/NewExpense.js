42532ae0fdc32639847f3f91637c7a4ca86f28c4
sbslc2000 <sbslc2000@gmail.com>
2023-02-13 21:45:23 +0900

    리액트 스터디 4일차 1. 리스트 렌더링과 key 2. 조건부 렌더링

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
+
 const NewExpense = (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
     const saveExpenseDateHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -9,12 +12,22 @@ const NewExpense = (props) => {
         };
 
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
 
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
+
     return (
-      <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={saveExpenseDateHandler} />
-      </div>
+        <div className='new-expense'>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDateHandler} onCancel={stopEditingHandler}/>}
+        </div>
     );
 };
 
